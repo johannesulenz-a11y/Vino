83ee7826185647196f8e65b7fd87a0b8136cbd40
johannesulenz-a11y <johannes.u.lenz@gmail.com>
2026-07-14 20:29:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vino-v1.9";
+const CACHE_NAME = "vino-v2.0";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
